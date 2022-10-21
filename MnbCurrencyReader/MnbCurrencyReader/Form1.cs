ef659c6416df0aa8c5c31d0b1fb1db5db2c7f883
csahokm <mihaly.csahok@stud.uni-corvinus.hu>
2022-10-21 10:17:26 +0200

    Third step. Connection with web service and simple request.

--- MnbCurrencyReader/MnbCurrencyReader/Form1.cs
+++ MnbCurrencyReader/MnbCurrencyReader/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MnbCurrencyReader.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,23 @@ namespace MnbCurrencyReader
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            CallWebService();
+        }
+        private void CallWebService()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient(); // client access
+            var request = new GetExchangeRatesRequestBody() // request parameters
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request); // make the request
+            var result = response.GetExchangeRatesResult; // store the result
         }
     }
 }
