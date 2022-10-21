e23b24d67fe2fab6b59021497bb9927f887e127f
csahokm <mihaly.csahok@stud.uni-corvinus.hu>
2022-10-21 10:56:22 +0200

    Sixth step. Create chart.

--- MnbCurrencyReader/MnbCurrencyReader/Form1.cs
+++ MnbCurrencyReader/MnbCurrencyReader/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace MnbCurrencyReader
@@ -22,6 +23,7 @@ namespace MnbCurrencyReader
             //CallWebService();
             dataGridView1.DataSource = Rates;
             XMLProcessing(CallWebService());
+            CreateChart();
         }
         private string CallWebService()
         {
@@ -55,6 +57,24 @@ namespace MnbCurrencyReader
                 }
 
             }
+        }
+        private void CreateChart()
+        {
+            chartRateData.DataSource = Rates;
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+            
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+            
         }
     }
 }
