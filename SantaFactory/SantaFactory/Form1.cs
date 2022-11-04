f934444c48b73a256ae9128f083f95ce6bfb2f5d
csahokm <mihaly.csahok@stud.uni-corvinus.hu>
2022-11-04 10:19:40 +0100

    Balls floating across the screen.

--- SantaFactory/SantaFactory/Form1.cs
+++ SantaFactory/SantaFactory/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SantaFactory.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,44 @@ namespace SantaFactory
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+
+        private BallFactory _factory;
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            int maxLeft = 0;
+            foreach(Ball b in _balls)
+            {
+                b.MoveBall();
+                if(b.Left > maxLeft)
+                {
+                    maxLeft = b.Left;
+                }
+            }
+            if(maxLeft > 1000)
+            {
+                Ball firstBall = _balls[0];
+                _balls.Remove(firstBall);
+                mainPanel.Controls.Remove(firstBall);
+            }
+        }
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            Ball b = Factory.CreateNew();
+            _balls.Add(b);
+            mainPanel.Controls.Add(b);
+            b.Left = -b.Width;
         }
     }
 }
