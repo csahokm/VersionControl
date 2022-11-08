d0316b9a147f5323fcada435e7aed0abdd454fef
csahokm <mihaly.csahok@stud.uni-corvinus.hu>
2022-11-08 11:37:21 +0100

    Calculating VaR.

--- ValueAtRiskAnalysis/ValueAtRiskAnalysis/Form1.cs
+++ ValueAtRiskAnalysis/ValueAtRiskAnalysis/Form1.cs
@@ -24,6 +24,25 @@ namespace ValueAtRiskAnalysis
             dataGridView1.DataSource = ticks;
 
             CreatePortfolio();
+
+            List<decimal> gains = new List<decimal>();
+            int interval = 30;
+            DateTime startDate = (from x in ticks select x.TradingDay).Min();
+            DateTime endDate = new DateTime(2016, 12, 30);
+            TimeSpan z = endDate - startDate;
+            for (int i = 0; i < z.Days - interval; i++)
+            {
+                decimal ny = GetPortfolioValue(startDate.AddDays(i + interval))
+                           - GetPortfolioValue(startDate.AddDays(i));
+                gains.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var gainsOrdered = (from x in gains
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(gainsOrdered[gainsOrdered.Count() / 5].ToString());
         }
         private void CreatePortfolio()
         {
